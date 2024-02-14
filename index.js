3804ece58433a30ebe6cc0b84b3516e16170edda
unknown <seke.lovro@gmail.com>
2024-02-14 15:30:13 +0100

    Initial commit of the QR-Code generator where I'm setting up the required packages, their usage format and using the default inquirer syntax to output the desired URL

---
+++ index.js
@@ -0,0 +1,29 @@
+//1. pokrenuti defaultne node pakete gdje -y oznacuje YES za sve prompte (npm init -y) 
+//2. instalirao inquirer, qr-image (npm i inquirer qr-image)
+//3. postavio tip importanja paketa u module u package.json ("type" : "module",)
+//4. koristenje defaultnog inquirer prompta iz dokumentacije za pribavljanje korisnickog unosa
+//5. upis "pitanja" u "message" JS objekt unutar .prompt bloka (message : "Enter your URL:", name : "URL")
+//6. upis naziva u "name"
+
+/* 1. Use the inquirer npm package to get user input. */
+
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+   { message : "Enter your URL:",
+   name : "URL" },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+// 3. Create a txt file to save the user input using the native fs node module.
