c189a86256cedc137e757b3c9535b7d8400ec0ed
unknown <seke.lovro@gmail.com>
2024-02-15 19:59:53 +0100

    Adding the qr-image package using module based usage format and implementing its function to generate the qr-code image which is getting saved on local storage using the file system native module

--- index.js
+++ index.js
@@ -5,9 +5,19 @@
 //5. upis "pitanja" u "message" JS objekt unutar .prompt bloka (message : "Enter your URL:", name : "URL")
 //6. upis naziva u "name"
 
-/* 1. Use the inquirer npm package to get user input. */
+//7. u qr-image dokumentaciji je require usage format tako da se to treba prenamijenit u module based
+//8. zalijepio ostatak koda za izradu qr-imagea
+//9. maknuo sam options svg jer se defaultno slika sprema u png formatu 
+//10. potrebno je importat fs (file system) native module jer treba spremiti qr.img file na nas komp
+
+
+// 1. Use the inquirer npm package to get user input.
+// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+// 3. Create a txt file to save the user input using the native fs node module.
 
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
@@ -16,7 +26,8 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.URL; //property ".URL" poslije answers mora biti isti kao gore u "name"  
-    console.log(url);
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -26,5 +37,4 @@ inquirer
     }
   });
 
-// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-// 3. Create a txt file to save the user input using the native fs node module.
+
